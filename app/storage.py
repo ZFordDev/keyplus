070bb5d47c21711ae6b1c33cd2774a4421e45685
ZFordDev <zforddev@gmail.com>
2026-06-26 06:57:13 +1000

    temp storage added

--- app/storage.py
+++ app/storage.py
@@ -5,19 +5,17 @@ app/storage.py
 communication to the rust backend
 """
 
-from keyplus_core import store_vault, load_vault
+from keyplus_core import temp_store_vault, temp_load_vault
 import json
 import os
 
 VAULT_PATH = "data/vault.json"
 
 def load_vault_data():
-    if not os.path.exists(VAULT_PATH):
-        return {"entries": []}
-    blob = load_vault(VAULT_PATH)
+    blob = temp_load_vault(VAULT_PATH)
     return json.loads(blob)
 
 def save_vault(data):
     os.makedirs("data", exist_ok=True)
     blob = json.dumps(data, indent=2)
-    store_vault(VAULT_PATH, blob)
+    temp_store_vault(VAULT_PATH, blob)
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,28 +1,18 @@
-"""
+/*
 core/lib.rs
 -----------
-
 Entry point for the front end
-"""
+*/
 
 use pyo3::prelude::*;
 use std::fs;
 
-#[pyfunction]
-fn store_vault(path: &str, json_blob: &str) -> PyResult<()> {
-    fs::write(path, json_blob)?;
-    Ok(())
-}
+mod temp_storage;
 
-#[pyfunction]
-fn load_vault(path: &str) -> PyResult<String> {
-    let data = fs::read_to_string(path)?;
-    Ok(data)
-}
 
 #[pymodule]
 fn keyplus_core(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(store_vault, py)?)?;
-    m.add_function(wrap_pyfunction!(load_vault, py)?)?;
+    m.add_function(wrap_pyfunction!(temp_storage::temp_store_vault, py)?)?;
+    m.add_function(wrap_pyfunction!(temp_storage::temp_load_vault, py)?)?;
     Ok(())
 }
